090b4e9fe9a4080d82f5cdd28476740f4f641983
Vlad <vl.sushkevich@gmail.com>
2025-12-02 23:05:56 +0300

    Added two calcs

--- test.py
+++ test.py
@@ -1,2 +1,58 @@
-print("Hello, it's me")
+"""Складывает, вычитает, умножает или делит два числа"""
+
+
+def calculator(first_number, second_number, operator):
+    if operator == '+':
+        return first_number + second_number
+    elif operator == '-':
+        return first_number - second_number
+    elif operator == '*':
+        return first_number * second_number
+    elif operator == '/':
+        if second_number == 0:
+            return 'Деление на ноль'
+        else:
+            return first_number / second_number
+    else:
+        return 'Неподдерживаемый оператор'
+
+first_number = int(input('Введите первое число: '))
+second_number = int(input('Введите второе число: '))
+operator = input('Введите оператор (+, -, *, /): ')
+result = calculator(first_number, second_number, operator)
+print(result)
+
+"""Складывает, вычитает, умножает или делит неограниченное количество чисел (только один оператор)"""
+
+
+def calculator(*numbers, operator):
+    numbers = numbers[0]
+    if operator == '+':
+        return sum(numbers)
+    elif operator == '-':
+        result = numbers[0]
+        for number in numbers[1:]:
+            result -= number
+        return result
+    elif operator == '*':
+        result = numbers[0]
+        for number in numbers[1:]:
+            result *= number
+        return result
+    elif operator == '/':
+        result = numbers[0]
+        for number in numbers[1:]:
+            if number == 0:
+                return 'Деление на ноль'
+            else:
+                result /= number
+        return result
+
+numbers = tuple(map(int, input('Введите числа через пробел: ').split()))
+operator = input('Введите оператор (+, -, *, /): ')
+print(calculator(numbers, operator = operator))
+
+
+
+
 
