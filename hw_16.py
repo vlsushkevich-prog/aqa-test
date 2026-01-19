25d9d77bdb08e89000011d31eacebc71f08ff7ec
Vlad <vl.sushkevich@gmail.com>
2026-01-20 01:13:07 +0300

    Added HW #16

--- hw_16.py
+++ hw_16.py
@@ -22,6 +22,9 @@ console_handler.setLevel(logging.INFO)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
 logger.addHandler(file_handler)
 logger.addHandler(console_handler)
 
